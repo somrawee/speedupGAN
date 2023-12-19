9a5fa566bae29574bdef68e26199e04cd3bdc971
Somrawee <somrawee_a@cmu.ac.th>
2023-12-19 15:01:56 +0700

    edit dcgans_v12

--- test_encoder.py
+++ test_encoder.py
@@ -6,4 +6,20 @@ encoding_dim = 32
 input_img = keras.Input(shape=(128,128,3))
 
 x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = layers.MaxPooling2D((2, 2), padding='same')(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
+
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = layers.UpSampling2D((2, 2))(x)
+x = layers.Conv2D(16, (3, 3), activation='relu')(x)
+x = layers.UpSampling2D((2, 2))(x)
+decoded = layers.Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+autoencoder = keras.Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
